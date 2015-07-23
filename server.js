15b8a1a16b1354ccf161fcab8bc21e093961c481
Ryan Groten <ryan.groten@stantec.com>
2015-07-23 10:36:31 -0600

    Add address:port values if not running in c9

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ var http = require("http");
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World\n');
-}).listen(process.env.PORT, process.env.IP);
+}).listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+    var addr = this.address();
+    console.log("Server listening at " + addr.address + ":" + addr.port);
+});
